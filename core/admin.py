a1efb174d3d5cd5f40a7a078a121156ca7d623d5
iarp <iarp.opensource@gmail.com>
2022-03-10 20:01:51 -0500

    fix(auth backend): staff and superusers can always login without staff check

--- core/admin.py
+++ core/admin.py
@@ -3,6 +3,5 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import Division, League, PermissionOverrides, Season, SubDivision, User
 
-
 admin.site.register((Season, League, Division, SubDivision, PermissionOverrides))
 admin.site.register(User, UserAdmin)
--- sportsnet/auth_backend.py
+++ sportsnet/auth_backend.py
@@ -5,5 +5,11 @@ class LoginRequiresStaffTypeWebAccessTrueBackend(ModelBackend):
     def authenticate(self, *args, **kwargs):
         user = super().authenticate(*args, **kwargs)
 
-        if user and user.staff_assignments.filter(type__web_access=True).exists():
+        if not user:
+            return
+
+        if user.is_staff or user.is_superuser:
+            return user
+
+        if user.staff_assignments.filter(type__web_access=True).exists():
             return user
